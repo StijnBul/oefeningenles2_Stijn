be6921697aa3ef7fd62904313b201ea83ad52675
Stijn <stijnbulckaen@msn.com>
2023-12-07 20:19:58 +0100

    Aangepaste oefening python

--- oefening2.py
+++ oefening2.py
@@ -1,6 +1,4 @@
 getal1=int(input('Voer eerste getal in:'))
 getal2=int(input('Voer tweede getal in:'))
-som=getal1+getal2
-product=getal1*getal2
-print('De som is:', som)
-print('De product is:', product)
+som=getal1+getal2+4
+print('De som is met 4 verhoogt:', som)
